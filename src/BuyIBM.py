2a70e7aa7eacef280ea5c43beb6d68a5050995a2
Yunlin Zhang <yunlinz@gmail.com>
2016-12-22 01:11:35 -0500

    added comments

--- src/BuyIBM.py
+++ src/BuyIBM.py
@@ -7,10 +7,12 @@ from src.Strategy import Strategy
 class BuyIbm(Strategy):
     def __init__(self):
         super().__init__()
+        # create these variables in addition to the standard ones that are created in the super class
         self.hist_quotes = None
         self.hist_signals = None
 
     def calculate_positions(self, quote, signals):
+        # persist the data frames
         if self.hist_quotes is None:
             self.hist_quotes = quote
         else:
@@ -19,14 +21,19 @@ class BuyIbm(Strategy):
             self.hist_signals = signals
         else:
             self.hist_signals.append(signals, ignore_index=True)
+
+        # don't do any actual processing and just return a single order in IBM
         return [('IBM', 1)]
 
     def analyze_trades_and_returns(self):
+        # the analysis just plots all the returns
         import matplotlib.pyplot as plt
         fig = plt.figure()
         fig.plot(self.returns)
         self.figures.append(fig)
 
 if __name__ == '__main__':
+    # instantiate BuyIbm class
     strat = BuyIbm()
+    # run the strategy
     strat.run_strategy()
