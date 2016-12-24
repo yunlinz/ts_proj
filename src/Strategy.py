02f0d1a69a3793366c9b0e51f19009701e331a0c
Yunlin Zhang <yunlinz@gmail.com>
2016-12-24 00:59:04 -0500

    swapped fundamentals to quarterly

--- src/Strategy.py
+++ src/Strategy.py
@@ -37,12 +37,13 @@ class Strategy(object):
         self.figures = []
         self.serial = None
         self.frequency = WEEK
+        self.text_data = {}
 
     def run_strategy(self):
         with BackTester() as bt:
             bt.set_universe(current_spx='../data/spx_constituents_20161216.csv'
                             , events='../data/spx_events.csv', quotes='../data/'
-                            , fundamentals='../data/fundamentals.csv'
+                            , fundamentals='../data/fundamentals_quarterly.csv'
                             , signals_file='../data/sp500_sectors.csv')
             res = None
             orders = []
@@ -85,6 +86,54 @@ class Strategy(object):
 
     def analyze_trades_and_returns(self):
         raise NotImplementedError
+        import pandas as pd
+        import numpy as np
+        from datetime import timedelta
+        import matplotlib.pyplot as plt
+
+        return_df = pd.DataFrame(data={
+            'Date': self.dates,
+            'Returns': self.returns
+        })
+        return_df['Date'] = return_df['Date'].apply(lambda x: x + timedelta(days=1))
+        return_df['DlrGrowth'] = 1 + return_df['Returns']
+        return_df.set_index('Date').sort_index()
+        return_df['CumuGrowth'] = return_df['DlrGrowth'].cumprod()
+        sp500 = pd.read_csv('../data/spx.csv', parse_dates=[0]).set_index('Date').sort_index()[['Close']]
+        sp500['returns'] = sp500['Close'].apply(np.log).diff().apply(np.exp) - 1
+        tbill = pd.read_csv('../data/tbill.csv', parse_dates=[0]).set_index('Date').sort_index()
+        tbill['Weekly'] = tbill['Tbill'] * 7.0 / 365.25 # actual/actual method
+
+        sp500 = sp500.join(tbill, how='inner', lsuffix='sp500', rsuffix='tbill')
+
+        strat = return_df.join(sp500, how='inner', lsuffix='strat', rsuffix='')
+
+        strat_annual_return = strat['CumuGrowth'].iloc[-1] ** (len(strat) * 7 / 365.25) - 1
+        strat_annual_vol = strat['Returns'].std() * np.sqrt(365.25 / 7)
+        strat_sharpe = (strat['Returns'] - strat['Weekly']).mean() * 365.25 / 7 / strat_annual_vol
+
+        strat['CumuGrowthSP'] = strat['Close'] / strat['Close'].iloc[0]
+        strat['SPWeekly'] = strat['Close'].apply(np.log).diff().apply(np.exp) - 1
+
+        sp500_annual_return = strat['CumuGrowthSP'].iloc[-1] ** (len(strat) * 7 / 365.25) - 1
+        sp500_annual_vol = strat['SPWeekly'].std() * np.sqrt(365.25 / 7)
+        sp500_sharpe = (strat['SPWeekly'] - strat['Weekly']).mean() * 365.25 / 7 / strat_annual_vol
+
+        self.text_data['Strategy Annualized Return'] = strat_annual_return
+        self.text_data['Strategy Annualized Vol'] = strat_annual_vol
+        self.text_data['Strategy Sharpe'] = strat_sharpe
+        self.text_data['SP500 Annualized Return'] = sp500_annual_return
+        self.text_data['SP500 Annualized Vol'] = sp500_annual_vol
+        self.text_data['SP500 Sharpe'] = sp500_sharpe
+
+        fig = plt.figure()
+        plt.plot(strat.index, strat['CumuGrowth'], strat['CumuGrowthSP'])
+        plt.xlabel('Time')
+        plt.ylabel('Value')
+        plt.title('Growth of $1')
+        plt.legend(['Strategy', 'S&P500'])
+        self.figures.append(fig)
+
 
     def save_all_data(self):
         import os, random, string
@@ -97,3 +146,6 @@ class Strategy(object):
         with open('../data/{}/return_series.csv'.format(name), 'w') as outstream:
             for d, r in zip(self.dates, self.returns):
                 outstream.write('{},{}\n'.format(d.strftime('%Y-%m-%d'), r))
+        with open('../data/{}/summary.txt'.format(name), 'w') as outstream:
+            for k, v in self.text_data.items():
+                outstream.write('{}:{}\n'.format(k, v))
